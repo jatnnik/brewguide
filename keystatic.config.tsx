214d26a4859d6f4c3eafadb0539569bd79c48489
Jannik <8103530+wh1zk1d@users.noreply.github.com>
2023-06-30 19:19:37 +0200

    switch from local keystatic to github

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -2,7 +2,11 @@ import { config, fields, collection } from "@keystatic/core"
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: {
+      owner: "wh1zk1d",
+      name: "brewguide",
+    },
   },
   collections: {
     brews: collection({
